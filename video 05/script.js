87bd0153a9b6b4db173d52d388696fbb137dd3e2
Shivedra123 <Shivendra@gmail.com>
2025-05-05 20:10:02 +0530

    video 05

---
+++ video 05/script.js
@@ -0,0 +1,23 @@
+//Logical Operators
+
+
+
+//and means    && 
+
+const age = 24;
+const gender = "male"
+
+if(age>= 18 && gender == 'male'){
+    console.log("your are eligble ")
+}
+
+
+// or means ||
+let room   = 10;
+let sex  = "male"
+
+if(room >= 16 || gender == 'male'){
+    console.log("yes coming")
+}
+
+//not menas !
